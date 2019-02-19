01328475b0ef63ccfad72a41187b91605f4f856b
Courtney Tyler <courtneytyler7@gmail.com>
2019-02-19 15:27:04 -0500

    set up job show component

--- src/Job/JobShow.js
+++ src/Job/JobShow.js
@@ -6,26 +6,22 @@ class JobShow extends Component {
     constructor() {
         super();
         this.state = {
-            job = []
+            job: []
         }
     }
 
-
-    // syntax error
     componentDidMount() {
         axios.get(`http://www.ourapi.com/{this.props.jobid}.json`)
           .then((res) => {
             this.setState({
-                job = res.data
+                job: res.data
              });
-          });
+          })
           .catch((err) => {
             console.log(err)
           })
       }
     
-    
-
     render() {
         return (
             <div key={this.state.job.position}>
@@ -33,9 +29,6 @@ class JobShow extends Component {
             <h2>{this.state.job.company}</h2>
             <p>{this.state.job.logoURL}</p>
             <p>{this.state.job.content}</p>
-            <Link to="/">
-           <a href="/"> <button value="delete" type="submit"onClick={this.handleDelete}>Delete</button></a>
-            </Link>
         </div>
         );
     }
