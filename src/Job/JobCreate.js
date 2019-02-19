5df6e29fa80633ca9816f639995e42901eb7a1f3
Courtney Tyler <courtneytyler7@gmail.com>
2019-02-19 12:53:15 -0500

    add job create and begin job show

---
+++ src/Job/JobCreate.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class JobCreate extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+        position: '',
+        company: '',
+        logoURL: '',
+        content: ''
+        };
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onChange = (e) => {
+        /*
+          Because we named the inputs to match their
+          corresponding values in state, it's
+          super easy to update the state
+        */
+        this.setState({ [e.target.name]: e.target.value });
+      }
+
+      onSubmit = (e) => {
+        e.preventDefault();
+        // get our form data out of state
+        const { position, company, logoURL, content } = this.state;
+
+        axios.post('/', { position, company, logoURL, content })
+          .then((result) => {
+            //access the results here....
+          });
+      }
+
+    render() {
+        const { position, company, logoURL, content } = this.state
+        return (
+            <form onSubmit={this.onSubmit}>
+            <input
+              type="text"
+              name="position"
+              value={position}
+              onChange={this.onChange}
+            />
+            <input
+              type="text"
+              name="company"
+              value={company}
+              onChange={this.onChange}
+            />
+            <input
+              type="text"
+              name="logoURL"
+              value={logoURL}
+              onChange={this.onChange}
+            />
+            <input
+              type="text"
+              name="content"
+              value={content}
+              onChange={this.onChange}
+            />
+            <button type="submit">Submit</button>
+            </form>
+
+        );
+    }
+}
+
+export default JobCreate;
+
+// Jobs:
+//     • content: String
+//     • position: String
+//     • company: String
+//     • location: String
+//     • companyLogo: String (Op)
+//     • user: 
+//         § userId: [ Linked Model ]
+//         § userName: [ Linked Model ]
+//     • createdAt:
+//         ▪ Type: Date
+//         ▪ Default: Date.now()
---
+++ src/Job/JobShow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class JobShow extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default JobShow;
