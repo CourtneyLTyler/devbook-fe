09b8ad626333d9f76e24fd4c4f563afa2decc3ca
Roger Campbell II <rc1336@gmail.com>
2019-02-20 13:12:19 -0500

    Able to render list of comments. Also tested to be sure that functionailty adds new comments when submitted which it does. Next Steps: I want to put a css border around the listed comments to help break it up and see what we are working with. Then: Need to add Update and Delete button and built out their functionality.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 // import UserForm from "./User/UserForm";
 import './App.css';
 import { Route, Link, Switch } from 'react-router-dom'
@@ -6,35 +7,38 @@ import { Route, Link, Switch } from 'react-router-dom'
 // import JobShow from '../src/Job/JobShow'
 import JobsList from '../src/Job/JobsList'
 import CreateComment from '../src/Comment/CommentCreate'
+import CommentList from './Comment/CommentList';
 // import axios from 'axios'
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
-      job: []
+      job: [],
+      comments: []
     }
   }
 
-  // componentDidMount() {
-  //   axios.get(`http://www.ourapi.com/{this.props.jobid}.json`)
-  //     .then((res) => {
-  //       this.setState({
-  //           job: res.data
-  //        });
-  //     })
-  //     .catch((err) => {
-  //       console.log(err)
-  //     })
-  // }
-
+  componentDidMount() {
+    Axios.get('http://localhost:3001/comments')
+      .then((res) => {
+        console.log(res)
+        this.setState({ comments: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
   render() {
     return (
       <div className="App">
         <p>Hello</p>
 
+
         <CreateComment />
+        <Route path='/comments' render={(routerProps) =>
+          <CommentList {...routerProps} {...this.state} />}
+        />
 
       </div>
     );
--- src/Comment/CommentList.js
+++ src/Comment/CommentList.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 
 class CommentList extends Component {
     render() {
+        const comments = this.props.comments.map(item => {
+            return (
+                <div className='indiv-comment' key={item._id}>
+                    <p>Comment: {item.content}</p>
+                    <p>Comment ID: {item._id}</p>
+                    <p>Likes: {item.likes}</p>
+                </div>
+            )
+        })
         return (
             <div>
-
+                {comments}
             </div>
         );
     }
