b0c984fa52c358bd05c4a36a3a6ef07f8c4c0248
Roger Campbell II <rc1336@gmail.com>
2019-02-20 22:50:53 -0500

    Merged dev branch into comments-component branch. Created CommentUpdate.js Component and added rcc boilerplate. Next Step: Build out the Update component. Then: Setup Route and Link functionality inside of CommentList.js and App.js.

---
+++ src/Comment/CommentUpdate.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CommentUpdate extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default CommentUpdate;
