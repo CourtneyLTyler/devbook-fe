635188c46e4a8ba0a9b83d724d448ee8c88217cf
Mifta Hamid <miftahamid@Miftas-MacBook-Air.local>
2019-02-21 09:50:25 -0500

    index.js changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-<<<<<<< HEAD
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-=======
-// import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-// import UserForm from "./user/userForm";
-import userProfile from "./user/UserProfile";
-import userForm from "./User/UserForm";
->>>>>>> user
 
 ReactDOM.render(
   <Router>
