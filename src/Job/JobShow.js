8c94bececa65524b0f96dcb5b6fe6de7780a7ee3
Roger Campbell II <rc1336@gmail.com>
2019-02-21 11:38:35 -0500

    Created a Job.css file. Added className='indiv-comment' to JobShow.js: 61. This will place border around the div to help me see. Next Steps: Build out the JobShow view so that it will attempt to rende user information. Then: Test to see if it rendering properly.

--- src/Job/JobShow.js
+++ src/Job/JobShow.js
@@ -5,76 +5,76 @@ import CommentCreate from '../Comment/CommentCreate'
 
 class JobShow extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            job: {}
-        }
-        this.handleDelete = this.handleDelete.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      job: {}
     }
+    this.handleDelete = this.handleDelete.bind(this)
+  }
 
-    componentDidMount() {
-        axios.get('http://localhost:3001/jobs/' + this.props.match.params.id)
-          .then((res) => {
-            this.setState({
-                job: res.data
-             });
-          })
-          .catch((err) => {
-            console.log(err)
-          })
-      }
+  componentDidMount() {
+    axios.get('http://localhost:3001/jobs/' + this.props.match.params.id)
+      .then((res) => {
+        this.setState({
+          job: res.data
+        });
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
-    //   handleDelete(){
-    //     console.log("deleting" + this.props.match.params.id);
-    //     axios.delete('http://localhost:3001/jobs/'+ this.props.match.params.id)
-    //       .then((res) => {
-    //         console.log(res.data)
-    //         this.context.history.push("/jobs")
-    //         this.setState({
-    //             job: {}
-    //         })
-    //       });
-    //     //   .catch((err)=> {
-    //     //     console.log(err)
-    //     //   })
-    //   }
-    
-      handleDelete = e => {
-        e.preventDefault();
-    
-        axios.delete(`http://localhost:3001/jobs/${this.state.job._id}`)
-          .then((res) => {
-            console.log(res.data)
-            this.setState({
-                job: {}
-            })
-            window.location.reload()
-          })
-          .catch((err) => {
-              console.log(err)
-          })
-      }
+  //   handleDelete(){
+  //     console.log("deleting" + this.props.match.params.id);
+  //     axios.delete('http://localhost:3001/jobs/'+ this.props.match.params.id)
+  //       .then((res) => {
+  //         console.log(res.data)
+  //         this.context.history.push("/jobs")
+  //         this.setState({
+  //             job: {}
+  //         })
+  //       });
+  //     //   .catch((err)=> {
+  //     //     console.log(err)
+  //     //   })
+  //   }
 
-    render() {
-        return (
-            <div key={this.state.job.position}>
-                <h1>Position: {this.state.job.position}</h1>
-                <h2>Company: {this.state.job.company}</h2>
-                <p>{this.state.job.logoURL}</p>
-                <p>Info: {this.state.job.content}</p>
-                <p>Location: {this.state.job.location}</p>
-                
-                <Link to={`/jobs/edit/${this.state.job._id}`}>
-                    <button value="update" type="update">Update</button>
-                </Link>
-                <Link to='/jobs'>
-                <button value="delete" type="submit"onClick={this.handleDelete}>Delete</button>
-                </Link>
-                < CommentCreate />
-            </div>
-        );
-    }
+  handleDelete = e => {
+    e.preventDefault();
+
+    axios.delete(`http://localhost:3001/jobs/${this.state.job._id}`)
+      .then((res) => {
+        console.log(res.data)
+        this.setState({
+          job: {}
+        })
+        window.location.reload()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <div key={this.state.job.position} className='indiv-comment'>
+        <h1>Position: {this.state.job.position}</h1>
+        <h2>Company: {this.state.job.company}</h2>
+        <p>{this.state.job.logoURL}</p>
+        <p>Info: {this.state.job.content}</p>
+        <p>Location: {this.state.job.location}</p>
+
+        <Link to={`/jobs/edit/${this.state.job._id}`}>
+          <button value="update" type="update">Update</button>
+        </Link>
+        <Link to='/jobs'>
+          <button value="delete" type="submit" onClick={this.handleDelete}>Delete</button>
+        </Link>
+        < CommentCreate />
+      </div>
+    );
+  }
 }
 
 export default JobShow;
--- src/user/UserForm.js
+++ src/user/UserForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import UserProfile from "./UserProfile";
+// import UserProfile from "./UserProfile";
 class UserForm extends Component {
   constructor() {
     super();
