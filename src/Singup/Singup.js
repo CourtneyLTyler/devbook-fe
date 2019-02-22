2d83175c72a5f27b6eb434b1a2b2ed554649861b
Mifta Hamid <miftahamid@Miftas-MacBook-Air.local>
2019-02-22 05:46:52 -0500

    Ok this should be good but it is not desplaying in the fromt but all the code is working

--- src/Singup/Singup.js
+++ src/Singup/Singup.js
@@ -1,39 +1,63 @@
 import React, { Component } from "react";
 
 class Singup extends Component {
-  constructor(props) {
-    super(props);
-
+  constructor() {
+    super();
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      passwordConfirm: "",
+      valid: " "
     };
+    this.ComponentPassword = this.ComponentPassword.bind(this);
+    this.ComponentPasswordConfirm = this.ComponentPasswordConfirm.bind(this);
+    this.ValidatorFunction = this.ValidatorFunction.bind(this);
   }
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
+  // write two functions for setState meaning when the user inputs the a value set that as the current value
+  ComponentPassword(event) {
+    this.setState({ password: event.target.value });
   }
 
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-  };
+  ComponentPasswordConfirm(event) {
+    this.setState({ passwordConfirm: event.target.value });
+  }
 
-  handleSubmit = event => {
-    event.preventDefault();
-  };
+  ValidatorFunction() {
+    if (this.state.password === this.state.passwordConfirm) {
+      // say valid
+      this.setState({
+        valid: "OOOH Look at you are a GENIE in a bottle you are Valid"
+      });
+    } else {
+      //say password do not match
+      this.setState({
+        valid: "COME ON GET YOUR STUFF TOGETHER, Password do not match"
+      });
+    }
+  }
+  // In here this is what you see in the brouser
   render() {
     return (
-      <div className="Login">
-        <form onSubmit={this.onSubmit}>
-          <label> Email: </label>
-          <input type="text" name="email" onChange={this.onChange} />
-          <label> Password: </label>
-          <input type="password" name="password" onChange={this.onChange} />
-        </form>
+      <div className="form">
+        <h1>Sign Up</h1>
+        <input type="text" placeholder="username" />
+        {/* this is looking for changes that have been made  */}
+        <input
+          onChange={this.ComponentPassword}
+          type="password"
+          placeholder="password"
+        />
+        <input
+          onChange={this.ComponentPasswordConfirm}
+          type="password"
+          placeholder="confim password"
+        />
+        {/* AFTER THEY CLICK THE SUBMITION BUTTON IT GO THRU THE FUNCTION AND CHECK THE PASSWORD IF IT IS THE SAME */}
+        <button onClick={this.ValidatorFunction}> submit </button>
+
+        <p>{this.state.valid}</p>
       </div>
     );
   }
 }
-
 export default Singup;
